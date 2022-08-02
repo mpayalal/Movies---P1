cbe027c4046970867e8ced28d60cd08190a76a4d
jf-puntocom <56199924+jf-puntocom@users.noreply.github.com>
2022-08-02 10:10:42 -0500

    about y color

--- movie/views.py
+++ movie/views.py
@@ -5,3 +5,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Maria Paula Ayala'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
