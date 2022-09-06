9d3d5d37c731e7d7bbb97ed1ad22fdf8beaaa8bc
jf-puntocom <56199924+jf-puntocom@users.noreply.github.com>
2022-09-06 09:50:13 -0500

    Visualizacion de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,10 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to= 'movie/images/')
+    url = models.URLField(blank=True) #significa que puede quedar en blanco, es decir, puede no estar este dato.
